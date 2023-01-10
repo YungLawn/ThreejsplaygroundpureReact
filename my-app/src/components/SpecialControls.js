7877f6440bb6a9c2ecdbd6cf5b27c0f4562d67bb
YungLawn <ccteelon@gmail.com>
2023-01-09 19:24:27 -0500

    ok

--- my-app/src/components/SpecialControls.js
+++ my-app/src/components/SpecialControls.js
@@ -1,28 +1,30 @@
 
 import React from 'react';
-import {OrbitControls} from '@react-three/drei';
+import {OrbitControls, MapControls} from '@react-three/drei';
 
 export default function SpecialControls() {
     return (
 
-        <OrbitControls 
-            minPolarAngle={Math.PI / 2}
-            maxPolarAngle={Math.PI / 1}
+        // <OrbitControls 
+        //     minPolarAngle={Math.PI / 2}
+        //     maxPolarAngle={Math.PI / 1}
+        //     minAzimuthAngle = {0}
+        //     maxAzimuthAngle = {0}
+        //     minDistance = {10}
+        //     maxDistance = {100}
+        //     panSpeed = {0.5}
+        //     rotateSpeed = {1}
+        //     zoomSpeed = {1}
+        //     location = {[0,0,0]}
+        // >
+        // </OrbitControls>
+
+        <MapControls> 
+            location ={[0,0,0]}
+            minPolarAngle={Math.PI / 2} 
+            maxPolarAngle={Math.PI / 1} 
             minAzimuthAngle = {0}
             maxAzimuthAngle = {0}
-            minDistance = {10}
-            maxDistance = {100}
-            panSpeed = {0.5}
-            rotateSpeed = {1}
-            zoomSpeed = {1}
-            location = {[0,0,0]}
-        >
-        </OrbitControls>
-
-        // <MapControls> 
-        //     location ={[0,0,0]}
-        //     {/* minPolarAngle={Math.PI / 2}
-        //     maxPolarAngle={Math.PI / 1} */}
-        // </MapControls>
+        </MapControls>
     )
 }
